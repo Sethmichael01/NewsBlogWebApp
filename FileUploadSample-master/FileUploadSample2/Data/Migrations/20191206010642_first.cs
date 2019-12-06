f3117bc4b6b71b15781231457635a7c397d94416
Sethmichael01 <49845849+Sethmichael01@users.noreply.github.com>
2019-12-06 03:41:54 +0100

    News WebApp

---
+++ FileUploadSample-master/FileUploadSample2/Data/Migrations/20191206010642_first.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FileUploadSample2.Data.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Alias",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTime",
+                table: "Products",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DateTime",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Alias",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ FileUploadSample-master/FileUploadSample2/Models/Product.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Runtime;
+
+namespace FileUploadSample2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int ProductCategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateTime { get; set; } 
+        public byte[] ProductPicture { get; set; }
+
+        [NotMapped]
+        public string ImageContentType { get; set; } = "image/png";
+
+        public string GetInLineImgSource()
+        {
+            if (ProductPicture == null || ImageContentType == null)
+            {
+                return null;
+            }
+            var base64Image = Convert.ToBase64String(ProductPicture);
+            return $"data:{ImageContentType};base64,{base64Image}";
+        }
+
+        [Display(Name = "Upload Product Picture")]
+        [NotMapped]
+        public IFormFile File
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+                try
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        value.CopyTo(memoryStream);
+                        ProductPicture = memoryStream.ToArray();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
